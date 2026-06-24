d1b1f6a0cd9556f9963abceda9f14fb9fa59eb7f
Michael Fichman <mfichman@gmail.com>
2026-06-24 11:25:21 -0400

    update IR error handling

---
+++ R/fetch_IR_lists_v2.R
@@ -0,0 +1,78 @@
+# Scrape MLB injury list, semi_join with rosters and produce a table of injuries.
+
+library(tidyverse)
+library(rvest)
+library(janitor)
+library(stringi)
+
+clean_player_name <- function(x) {
+  x %>%
+    str_to_lower() %>%
+    stringi::stri_trans_general("Latin-ASCII") %>%
+    str_replace_all("[^a-z ]", "") %>%
+    str_squish()
+}
+
+injured_on_list <- tryCatch({
+  
+  url <- "https://www.espn.com/mlb/injuries"
+  
+  page <- read_html(
+    httr::GET(
+      url,
+      httr::user_agent("Mozilla/5.0")
+    )
+  )
+  
+  injured_table <- page %>%
+    html_elements(".ResponsiveTable") %>%
+    map_dfr(function(x) {
+      team <- x %>% html_element(".Table__Title") %>% html_text2()
+      
+      tbl <- x %>%
+        html_element("table") %>%
+        html_table(fill = TRUE) %>%
+        clean_names()
+      
+      tbl %>% mutate(team = team, .before = 1)
+    }) %>%
+    rename(player = name) %>%
+    mutate(player_key = clean_player_name(player))
+  
+  the_list_clean <- pitcher_roster %>%
+    select(Owner, Name, Position) %>%
+    bind_rows(
+      batter_roster %>% select(Owner, Name, Position)
+    ) %>%
+    mutate(player_key = clean_player_name(Name))
+  
+  injured_table %>%
+    inner_join(the_list_clean, by = "player_key") %>%
+    filter(status != "Day-To-Day") %>%
+    select(Name, Owner, team, pos, Position, status, est_return_date, comment) %>%
+    rename(
+      Player = Name,
+      Team = team,
+      `Roster Position` = Position,
+      Position = pos,
+      Status = status,
+      `Estimated Return` = est_return_date,
+      Details = comment
+    ) %>%
+    arrange(Owner)
+  
+}, error = function(e) {
+  
+  message("Could not fetch ESPN injury table: ", e$message)
+  
+  tibble(
+    Player = character(),
+    Owner = character(),
+    Team = character(),
+    Position = character(),
+    `Roster Position` = character(),
+    Status = character(),
+    `Estimated Return` = character(),
+    Details = character()
+  )
+})
